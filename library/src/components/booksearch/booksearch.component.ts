581ef590804b894e0101cd46894f9706207df390
Baba Fakruddin <fakruddinbaba16@gmail.com>
2021-02-04 19:16:00 +0530

    booking finding work

--- library/src/components/booksearch/booksearch.component.ts
+++ library/src/components/booksearch/booksearch.component.ts
@@ -2,43 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { BookService } from '../book.service';
 
-
 @Component({
   selector: 'app-booksearch',
   templateUrl: './booksearch.component.html',
-  styleUrls: ['./booksearch.component.scss']
+  styleUrls: ['./booksearch.component.scss'],
 })
 export class BooksearchComponent implements OnInit {
-query : FormControl= new FormControl();
-items: any = [];
-img: any;
-title: any;
-subtitle: any;
-thumb:any;
-  constructor(private book: BookService) { }
+  query: FormControl = new FormControl();
+  items: any = [];
+  img: any;
+  title: any;
+  subtitle: any;
+  thumb: any;
+  constructor(private book: BookService) {}
 
-  ngOnInit(): void {
-    
-
+  ngOnInit(): void {}
+  OnClick() {
+    this.book.get(this.query.value).subscribe((result: any) => {
+      console.log(result.items);
+      var listitems = result.items.map((o: any) => {
+        return o.volumeInfo;
+      });
+      this.items.push(listitems);
+    });
+    console.log(this.items);
   }
-  OnClick(){
-  this.book.get(this.query.value).subscribe((result:any) => {
-    console.log(result.items);
-    result.items.filter((o:any) => {
-      let itemlist = [];
-      let volumeInfo = o.volumeInfo;
-      this.title = volumeInfo.title;
-      itemlist.push(this.title);
-      this.subtitle = volumeInfo.subtitle;
-      itemlist.push(this.subtitle);
-      this.img = volumeInfo.imageLinks;
-      this.thumb =this.img.thumbnail;
-      itemlist.push(this.thumb);
-      return this.items.push(itemlist);
-    })
-    
-  })
-  
-  console.log(this.items);
-}
 }
